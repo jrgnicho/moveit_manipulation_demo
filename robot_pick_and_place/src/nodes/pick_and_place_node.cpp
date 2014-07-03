9333d47fe29a9533795c49bf7b97d469f55a4655
jrgnicho <jrgnicho@gmail.com>
2014-07-03 17:58:35 -0500

    removed all individual method calls and from the node

--- robot_pick_and_place/src/nodes/pick_and_place_node.cpp
+++ robot_pick_and_place/src/nodes/pick_and_place_node.cpp
@@ -5,16 +5,6 @@ using namespace robot_pick_and_place;
 // =============================== Main Thread ===============================
 int main(int argc,char** argv)
 {
-  geometry_msgs::Pose box_pose;
-  std::vector<geometry_msgs::Pose> pick_poses, place_poses;
-
-  /* =========================================================================================*/
-  /*	INITIALIZING ROS NODE
-      Goal:
-      - Observe all steps needed to properly initialize a ros node.
-      - Look into the 'cfg' member of PickAndPlace to take notice of the parameters that
-        are available for the rest of the program. */
-  /* =========================================================================================*/
 
   // ros initialization
   ros::init(argc,argv,"pick_and_place_node");
@@ -25,99 +15,10 @@ int main(int argc,char** argv)
   // creating pick and place application instance
   PickAndPlace application;
 
-  // reading parameters
-  if(application.cfg.init())
+  if(application.init())
   {
-    ROS_INFO_STREAM("Parameters successfully read");
+	  application.run();
   }
-  else
-  {
-    ROS_ERROR_STREAM("Parameters not found");
-    return 0;
-  }
-
-  // marker publisher
-  application.marker_publisher = nh.advertise<visualization_msgs::Marker>(
-		  application.cfg.MARKER_TOPIC,1);
-
-  // planning scene publisher
-  application.planning_scene_publisher = nh.advertise<moveit_msgs::PlanningScene>(
-  		application.cfg.PLANNING_SCENE_TOPIC,1);
-
-  // moveit interface
-  application.move_group_ptr = MoveGroupPtr(
-		  new move_group_interface::MoveGroup(application.cfg.ARM_GROUP_NAME));
-
-  // motion plan client
-  application.motion_plan_client = nh.serviceClient<moveit_msgs::GetMotionPlan>(application.cfg.MOTION_PLAN_SERVICE);
-
-  // transform listener
-  application.transform_listener_ptr = TransformListenerPtr(new tf::TransformListener());
-
-  // marker publisher (rviz visualization)
-  application.marker_publisher = nh.advertise<visualization_msgs::Marker>(
-		  application.cfg.MARKER_TOPIC,1);
-
-  // target recognition client (perception)
-  application.target_recognition_client = nh.serviceClient<robot_pick_and_place::GetTargetPose>(
-		  application.cfg.TARGET_RECOGNITION_SERVICE);
-
-  application.grasp_pose_client = nh.serviceClient<handle_detector::GraspPoseCandidates>(
-		  application.cfg.GRASP_POSES_SERVICE);
-
-  // grasp action client (vacuum gripper)
-  application.grasp_action_client_ptr = GraspActionClientPtr(
-		  new GraspActionClient(application.cfg.GRASP_ACTION_NAME,true));
-
-  // ik client (inverse kinematics)
-  application.ik_client = nh.serviceClient<moveit_msgs::GetPositionIK>(application.cfg.IK_SERVICE);
-
-  // robot state check client
-  application.state_check_client = nh.serviceClient<moveit_msgs::GetStateValidity>("check_state_validity");
-
-
-  // waiting to establish connections
-  while(ros::ok() &&
-      !application.grasp_action_client_ptr->waitForServer(ros::Duration(2.0f)))
-  {
-    ROS_INFO_STREAM("Waiting for grasp action servers");
-  }
-
-  if(ros::ok() && !application.grasp_pose_client.waitForExistence(ros::Duration(2.0f))
-		  && !application.ik_client.waitForExistence(ros::Duration(2.0f))
-		  && !application.state_check_client.waitForExistence(ros::Duration(2.0f)))
-  {
-	  ROS_INFO_STREAM("Waiting for services");
-  }
-
-
-  /* ========================================*/
-  /* Pick & Place Tasks                      */
-  /* ========================================*/
-
-  // move to a "clear" position
-  application.move_to_wait_position();
-
-  // turn off vacuum gripper
-  application.set_gripper(false);
-
-  // get the box position and orientation
-  box_pose = application.detect_target_pose();
-
-  // build a sequence of poses to "pick" the box
-  pick_poses = application.create_pick_moves(box_pose);
-
-  // plan/execute the sequence of "pick" moves
-  application.pickup_box(pick_poses,box_pose);
-
-  // build a sequence of poses to "place" the box
-  place_poses = application.create_place_moves();
-
-  // plan/execute the "place" moves
-  application.place_box(place_poses,box_pose);
-
-  // move back to the "clear" position
-  application.move_to_wait_position();
 
   return 0;
 }
