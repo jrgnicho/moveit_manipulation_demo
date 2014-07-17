a90c971a5b3d6231ff149211ff5ca4c2a33baeea
ros-industrial <ros-industrial@swri.org>
2014-07-17 17:53:11 -0500

    added methos for manipulation bits into bytes and viceversa

--- robot_io/src/nodes/soem_manager.cpp
+++ robot_io/src/nodes/soem_manager.cpp
@@ -6,6 +6,10 @@
  */
 #include <sstream>
 #include <ros/ros.h>
+#include <robot_io/SoemIO.h>
+#include <boost/assign.hpp>
+#include <boost/assign/list_of.hpp>
+#include <boost/assign/std/vector.hpp>
 
 #include <stdio.h>
 #include <string.h>
@@ -23,7 +27,7 @@
 #include <ethercat_soem/ethercatconfig.h>
 #include <ethercat_soem/ethercatprint.h>
 
-
+using namespace boost::assign;
 
 #define EC_TIMEOUTMON 500
 
@@ -46,8 +50,6 @@ public:
 	bool init()
 	{
 
-		char ifname[1024];
-
 		if(load_parameters())
 		{
 			ROS_INFO_STREAM("Parameters loaded");
@@ -58,7 +60,64 @@ public:
 			return false;
 		}
 
+		if(!init_soem())
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	void run()
+	{
+
+
+		ROS_INFO_STREAM("test bit|byte conversions");
+		test_conversions();
+
+		if(init())
+		{
+		}
+	}
+
+protected:
+
+	bool load_parameters()
+	{
+		ros::NodeHandle ph("~");
+		bool success =  ph.getParam("ifname",ifname_) &&
+				ph.getParam("device_name",device_name_);
+		return success;
+	}
+
+	void list_devices()
+	{
+		if(ec_slavecount > 0)
+		{
+			std::stringstream ss;
+			for(int i =0;i < ec_slavecount;i++)
+			{
+				ec_slavet &sl = ec_slave[i];
+				ss<<"\tSlave["<<i+1<<"]\n";
+				ss<<"\t- Name: "<<std::string(sl.name)<<"\n";
+				ss<<"\t- Input Bytes: "<<sl.Ibytes<<"\n";
+				ss<<"\t- Input Bits: "<<sl.Ibits<<"\n";
+				ss<<"\t- Output Bytes: "<<sl.Obytes<<"\n";
+				ss<<"\t- Output Bits: "<<sl.Obits<<"\n";
+			}
+
+			ROS_INFO_STREAM("Ethercat devices found:\n"<<ss.str());
+		}
+		else
+		{
+			ROS_ERROR_STREAM("No Ethercat devices were found");
+		}
+	}
+
+	bool init_soem()
+	{
 		// copying characters
+		char ifname[1024];
 		strncpy(ifname,ifname_.c_str(),sizeof(ifname));
 		ifname[sizeof(ifname)-1] = 0;
 
@@ -117,58 +176,89 @@ public:
 		}
 		else
 		{
-			ROS_ERROR_STREAM("SOEM failed on "<<ifname_ <<" failed to initialize");
+			ROS_ERROR_STREAM("SOEM on "<<ifname_ <<" failed to initialize");
 			return 	false;
 		}
 
 		return true;
 	}
 
-	void run()
+	void test_conversions()
 	{
+		std::vector<uint8> bytes;
+		uint8 result;
+		std::vector<uint8_t> bit_array;
 
-		ROS_INFO_STREAM("test soem");
-		if(init())
+		// creating array of test bytes
+		bytes.push_back(12);
+		bytes.push_back(52);
+		bytes.push_back(14);
+		bytes.push_back(72);
+		bytes.push_back(126);
+
+
+		for(int i = 0;i < bytes.size();i++)
 		{
+			byte_to_bits(bytes[i],bit_array);
+			result = bits_to_byte(bit_array);
 
+			ROS_INFO_STREAM("Byte "<<(int)bytes[i]<<" -> "<<bits_to_str(bit_array)<<
+					" -> "<<(int)result);
 		}
+
+
+
+
+	}
+
+	uint8 bits_to_byte(const std::vector<uint8_t>& bit_array)
+	{
+		uint8 byte = 0;
+		for(int i = 0;i < bit_array.size(); i++)
+		{
+			byte = bit_array[i]<<i | byte;
+		}
+
+		return byte;
+	}
+
+	void byte_to_bits(uint8 byte, std::vector<uint8_t>& bit_array)
+	{
+		bit_array.resize(8);
+		for(int i=0;i < 8;i++)
+		{
+			uint8_t val = 1 << i;
+			bit_array[i] = (byte & (val)) == val ? 1 : 0;
+		}
+
+	}
+
+	std::string bits_to_str(const std::vector<uint8_t>& bit_array)
+	{
+		std::stringstream ss;
+		ss<<"[";
+		for(int i=0;i < 8;i++)
+		{
+			ss<<(int)bit_array[i]<<" ";
+		}
+		ss<<"]";
+		return ss.str();
+	}
+
+	void test_open_gripper()
+	{
+		robot_io::RegisterData out1; //action requested
+		robot_io::RegisterData out3; // position requested
+
+		// action requested
+		out1.bits = list_of(1)(0)(0)(0)(0)(0)(0);
+
 	}
 
 protected:
 
-	bool load_parameters()
-	{
-		ros::NodeHandle ph("~");
-		bool success =  ph.getParam("ifname",ifname_) &&
-				ph.getParam("device_name",device_name_);
-		return success;
-	}
-
-	void list_devices()
-	{
-		if(ec_slavecount > 0)
-		{
-			std::stringstream ss;
-			for(int i =0;i < ec_slavecount;i++)
-			{
-				ec_slavet &sl = ec_slave[i];
-				ss<<"\tSlave["<<i+1<<"]\n";
-				ss<<"\t- Name: "<<std::string(sl.name)<<"\n";
-				ss<<"\t- Input Bytes: "<<sl.Ibytes<<"\n";
-				ss<<"\t- Input Bits: "<<sl.Ibits<<"\n";
-				ss<<"\t- Output Bytes: "<<sl.Obytes<<"\n";
-				ss<<"\t- Output Bits: "<<sl.Obits<<"\n";
-			}
-
-			ROS_INFO_STREAM("Ethercat devices found:\n"<<ss.str());
-		}
-		else
-		{
-			ROS_ERROR_STREAM("No Ethercat devices were found");
-		}
-	}
-
-protected:
+	// ros comm
+	ros::ServiceServer soem_io_server_;
 
 	// parameters
 	std::string ifname_; // usually eth0
